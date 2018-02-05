04ff75486d8d2a6fe42f75b072253749ea196de6
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-05 17:19:52 +0000

    Car border is awesome

--- lib/app.rb
+++ lib/app.rb
@@ -13,3 +13,12 @@ end
 get "/bensmagicbox" do
   "My box is full of magic things"
 end
+
+get "/cat" do
+"<div style='border: 5px dashed red'>
+  <img src='http://bit.ly/1eze8aE'/>
+</div>"
+end
+
+
+# send_file('/root/dev/notes/images/'+params[:file], :disposition => 'inline')
